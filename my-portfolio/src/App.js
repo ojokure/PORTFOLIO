c8a0c592c51747919452094ff0c2caceccdf9b81
oladimeji-ojo <ojokuredim@gmail.com>
2020-03-30 13:55:26 +0100

    landing

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Layout, Navigation, Header, Drawer, Content } from "react-mdl";
+// import wallp from "./images/wallpaper.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="demo-big-content">
+      <Layout>
+        <Header title="Title" scroll>
+          <Navigation>
+            <a href="#">Link</a>
+            <a href="#">Link</a>
+            <a href="#">Link</a>
+            <a href="#">Link</a>
+          </Navigation>
+        </Header>
+        <Drawer title="Title">
+          <Navigation>
+            <a href="#">Link</a>
+            <a href="#">Link</a>
+            <a href="#">Link</a>
+            <a href="#">Link</a>
+          </Navigation>
+        </Drawer>
+        <Content>
+          <div className="page-content" />
+        </Content>
+      </Layout>
     </div>
   );
 }
---
+++ my-portfolio/src/components/landingpage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function LandingPage() {
+  return (
+    <div>
+      <h1> Lading Page</h1>
+    </div>
+  );
+}
